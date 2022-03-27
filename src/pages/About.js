ded3f7650fa24e3f4a76761d8e8f1cc742a8be9d
Ekuremaro <86929331+Ekuremaro@users.noreply.github.com>
2022-03-27 16:12:12 +0100

    created pages and components, created error and about pages

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis,
+        praesentium natus temporibus obcaecati minima impedit placeat iure
+        quaerat quia aspernatur omnis doloribus necessitatibus minus quibusdam
+        rem sit sed provident optio adipisci explicabo! Deleniti nulla natus
+        pariatur, quas labore totam nostrum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
