3ae19c864dcd3e41d14397d4279e61835cf217eb
Ekuremaro <86929331+Ekuremaro@users.noreply.github.com>
2022-03-28 17:43:15 +0100

    added single cocktail component and display

--- src/context.js
+++ src/context.js
@@ -12,12 +12,14 @@ const AppProvider = ({ children }) => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
-      const drinks = await response.json();
-      console.log(drinks);
+      const data = await response.json();
+
+      const { drinks } = data;
       if (drinks) {
         const newCocktails = drinks.map((item) => {
           const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
             item;
+
           return {
             id: idDrink,
             name: strDrink,
@@ -26,6 +28,8 @@ const AppProvider = ({ children }) => {
             glass: strGlass,
           };
         });
+
+        setCocktails(newCocktails);
       } else {
         setCocktails([]);
       }
